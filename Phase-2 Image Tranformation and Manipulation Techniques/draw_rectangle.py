7fa9e0f414c2e2347b346c73293ed9794c4b3901
Muhammad-Javed2005 <muhammadjaved.tech5@gmail.com>
2026-01-26 22:58:47 +0500

    Draw Rectangle

---
+++ Phase-2 Image Tranformation and Manipulation Techniques/draw_rectangle.py
@@ -0,0 +1,24 @@
+# **********************************************************
+# *** Engr. Muhammad Javed ***
+# **********************************************************
+
+import cv2
+
+image = cv2.imread("E:\AI and Data Science\Open CV With Engr.Muhammad Javed\Output_python.png")
+
+if image is None:
+    print("Image not found, please check the path.") 
+
+else:
+    print("Image loaded successfully.")
+
+    plt1 = (50 , 100)
+    plt2 = (200 , 250)
+    color = (0 , 255 , 0)
+    thickness = 4
+
+    cv2.rectangle(image , plt1 , plt2 , color , thickness)
+
+    cv2.imshow("Draw rectangle on Image", image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
