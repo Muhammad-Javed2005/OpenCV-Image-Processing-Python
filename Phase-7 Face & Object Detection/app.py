72775e3f2245dcd70aaa8427adb91d43359de28f
Muhammad-Javed2005 <muhammadjaved.tech5@gmail.com>
2026-02-08 19:00:52 +0500

    App

---
+++ Phase-7 Face & Object Detection/app.py
@@ -0,0 +1,21 @@
+import cv2
+
+face_cascade = cv2.CascadeClassifier("Phase-7 Face & Object Detection\haarcascade_frontalface_alt2.xml")
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)
+
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+
+    cv2.imshow("Face Detection", frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
